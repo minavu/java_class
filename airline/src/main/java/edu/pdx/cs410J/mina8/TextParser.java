5c126e1d1ed5aa8bf69865f0da070affa721ad66
Mina <minavu@Minas-MacBook-Pro.local>
2022-01-25 15:29:24 -0800

    Progressing on TextParser class

--- airline/src/main/java/edu/pdx/cs410J/mina8/TextParser.java
+++ airline/src/main/java/edu/pdx/cs410J/mina8/TextParser.java
@@ -3,10 +3,9 @@ package edu.pdx.cs410J.mina8;
 import edu.pdx.cs410J.AirlineParser;
 import edu.pdx.cs410J.ParserException;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TextParser implements AirlineParser<Airline> {
   private final Reader reader;
@@ -15,19 +14,34 @@ public class TextParser implements AirlineParser<Airline> {
     this.reader = reader;
   }
 
+  public TextParser(String filename) throws FileNotFoundException {
+    this.reader = new FileReader(filename);
+  }
+
   @Override
   public Airline parse() throws ParserException {
     try (
       BufferedReader br = new BufferedReader(this.reader)
     ) {
+      Airline airline = null;
 
       String airlineName = br.readLine();
+      String[] parsedLine = airlineName.split("\\|");
 
       if (airlineName == null) {
         throw new ParserException("Missing airline name");
       }
 
-      return new Airline(airlineName);
+      airline = new Airline(parsedLine[0]);
+      Flight flight = new Flight(new ArrayList<>(Arrays.asList(parsedLine)));
+      airline.addFlight(flight);
+      while (br.ready()) {
+        airlineName = br.readLine();
+        parsedLine = airlineName.split("\\|");
+        flight = new Flight(new ArrayList<>(Arrays.asList(parsedLine)));
+        airline.addFlight(flight);
+      }
+      return airline;
 
     } catch (IOException e) {
       throw new ParserException("While parsing airline text", e);
--- airline/src/test/java/edu/pdx/cs410J/mina8/TextParserTest.java
+++ airline/src/test/java/edu/pdx/cs410J/mina8/TextParserTest.java
@@ -3,12 +3,14 @@ package edu.pdx.cs410J.mina8;
 import edu.pdx.cs410J.ParserException;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+//import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TextParserTest {
@@ -30,5 +32,72 @@ public class TextParserTest {
 
     TextParser parser = new TextParser(new InputStreamReader(resource));
     assertThrows(ParserException.class, parser::parse);
+  }
+
+  @Test
+  void nonexistentFileNameThrowsFileNotFoundException() {
+    String filename = "notAFile.txt";
+    assertThrows(FileNotFoundException.class, () -> new TextParser(filename));
+  }
+
+  @Test
+  void existingFileNameCanBeAccessed() {
+    String filename = "resources/isAFile.txt";
+    TextParser parser = null;
+    try {
+      parser = new TextParser(filename);
+    } catch (FileNotFoundException e) {
+    }
+    assertThat(parser, notNullValue());
+  }
+
+  @Test
+  void existingFileCanBeReadUsingDelimiter() {
+    String filename = "resources/isAFile.txt";
+    TextParser parser = null;
+    try {
+      parser = new TextParser(filename);
+    } catch (FileNotFoundException e) {
+    }
+    Airline airline = null;
+    try {
+      airline = parser.parse();
+    } catch (ParserException e) {
+    }
+    assertThat(airline.getName(), equalTo("abc"));
+  }
+
+  @Test
+  void fileWithValidFlightInfoCanCreateANewFlight() {
+    String filename = "resources/isAFile.txt";
+    TextParser parser = null;
+    try {
+      parser = new TextParser(filename);
+    } catch (FileNotFoundException e) {
+    }
+    Airline airline = null;
+    try {
+      airline = parser.parse();
+    } catch (ParserException e) {
+    }
+    ArrayList<Flight> flights = (ArrayList<Flight>) airline.getFlights();
+    assertThat(flights.get(0).toString(), containsString("Flight 123"));
+  }
+
+  @Test
+  void fileWithMultipleFlightsCanAllBeCreatedAndAddedToAirline() {
+    String filename = "resources/isAFile.txt";
+    TextParser parser = null;
+    try {
+      parser = new TextParser(filename);
+    } catch (FileNotFoundException e) {
+    }
+    Airline airline = null;
+    try {
+      airline = parser.parse();
+    } catch (ParserException e) {
+    }
+    ArrayList<Flight> flights = (ArrayList<Flight>) airline.getFlights();
+    assertThat(flights.size(), greaterThan(1));
   }
 }
