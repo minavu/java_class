5b9621dca3806e97c8acb5c3eb100f3f6afef637
Mina <minavu@Minas-MacBook-Pro.local>
2022-02-12 15:47:18 -0800

    XmlDumper class done-ish.

--- airline/src/main/java/edu/pdx/cs410J/mina8/XmlDumper.java
+++ airline/src/main/java/edu/pdx/cs410J/mina8/XmlDumper.java
@@ -7,12 +7,17 @@ import org.w3c.dom.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 import java.net.MalformedURLException;
+import java.util.Calendar;
 
 public class XmlDumper implements AirlineDumper<Airline> {
     private final AirlineXmlHelper helper;
     private final Writer writer;
+    String systemID = "http://www.cs.pdx.edu/~whitlock/dtds/airline.dtd";
 
     public XmlDumper(Writer writer) throws IOException {
         helper = new AirlineXmlHelper();
@@ -21,19 +26,81 @@ public class XmlDumper implements AirlineDumper<Airline> {
 
     @Override
     public void dump(Airline airline) throws IOException {
+        Document doc = createXmlDocument();
+        try {
+            Element air = doc.getDocumentElement();
 
+            Element name = doc.createElement("name");
+            name.appendChild(doc.createTextNode(airline.getName()));
+            air.appendChild(name);
+
+            airline.getFlights().forEach(flight -> {
+                Element flightElement = doc.createElement("flight");
+                Element number = doc.createElement("number");
+                number.appendChild(doc.createTextNode(String.valueOf(flight.getNumber())));
+                flightElement.appendChild(number);
+
+                Element src = doc.createElement("src");
+                src.appendChild(doc.createTextNode(flight.getSource()));
+                flightElement.appendChild(src);
+
+                Element depart = doc.createElement("depart");
+                Element d_date = doc.createElement("date");
+                Calendar d_calendar = Calendar.getInstance();
+                d_calendar.setTime(flight.getDeparture());
+                d_date.setAttribute("day", String.valueOf(d_calendar.get(Calendar.DAY_OF_MONTH)));
+                d_date.setAttribute("month", String.valueOf(d_calendar.get(Calendar.MONTH +1)));
+                d_date.setAttribute("year", String.valueOf(d_calendar.get(Calendar.YEAR)));
+                Element d_time = doc.createElement("time");
+                d_time.setAttribute("hour", String.valueOf(d_calendar.get(Calendar.HOUR_OF_DAY)));
+                d_time.setAttribute("minute", String.valueOf(d_calendar.get(Calendar.MINUTE)));
+                depart.appendChild(d_date);
+                depart.appendChild(d_time);
+                flightElement.appendChild(depart);
+
+                Element dest = doc.createElement("dest");
+                dest.appendChild(doc.createTextNode(flight.getDestination()));
+                flightElement.appendChild(dest);
+
+                Element arrive = doc.createElement("arrive");
+                Element a_date = doc.createElement("date");
+                Calendar a_calendar = Calendar.getInstance();
+                a_calendar.setTime(flight.getArrival());
+                a_date.setAttribute("day", String.valueOf(a_calendar.get(Calendar.DAY_OF_MONTH)));
+                a_date.setAttribute("month", String.valueOf(a_calendar.get(Calendar.MONTH +1)));
+                a_date.setAttribute("year", String.valueOf(a_calendar.get(Calendar.YEAR)));
+                Element a_time = doc.createElement("time");
+                a_time.setAttribute("hour", String.valueOf(a_calendar.get(Calendar.HOUR_OF_DAY)));
+                a_time.setAttribute("minute", String.valueOf(a_calendar.get(Calendar.MINUTE)));
+                arrive.appendChild(a_date);
+                arrive.appendChild(a_time);
+                flightElement.appendChild(arrive);
+
+                air.appendChild(flightElement);
+            });
+
+
+        } catch (DOMException e) {
+
+        }
+        try {
+            Source src = new DOMSource(doc);
+            Result res = new StreamResult(writer);
+            TransformerFactory xFactory = TransformerFactory.newInstance();
+            Transformer xForm = xFactory.newTransformer();
+            xForm.setOutputProperty(OutputKeys.INDENT, "yes");
+            xForm.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, systemID);
+            xForm.transform(src, res);
+        } catch (TransformerConfigurationException e) {
+
+        } catch (TransformerException e) {
+
+        }
     }
 
     public Document createXmlDocument() {
-        String systemID = null;
         Document doc = null;
 
-        try {
-            File dtd = new File("airline.dtd");
-            systemID = dtd.toURL().toString();
-        } catch (MalformedURLException e) {
-            throw new IllegalArgumentException("Airline DTD cannot be parsed into URl.");
-        }
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             factory.setValidating(true);
@@ -46,4 +113,5 @@ public class XmlDumper implements AirlineDumper<Airline> {
         }
         return doc;
     }
+
 }
--- airline/src/test/java/edu/pdx/cs410J/mina8/XmlDumperTest.java
+++ airline/src/test/java/edu/pdx/cs410J/mina8/XmlDumperTest.java
@@ -6,6 +6,8 @@ import org.w3c.dom.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -13,7 +15,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class XmlDumperTest {
     File testFile = new File("test.xml");
-    String dtd = "http://www.cs.pdx.edu/~whitlock/dtds/airline.dtd";
+    ArrayList<String> argsListWithAllArgsSampler1 = new ArrayList<>(Arrays.asList("abc", "123", "pdx", "1/1/2022", "9:15", "am", "hnl", "1/1/2022", "11:30", "pm"));
+    ArrayList<String> argsListWithAllArgsSampler2 = new ArrayList<>(Arrays.asList("abc", "123", "mia", "1/1/2022", "9:15", "am", "hnl", "1/1/2022", "11:30", "pm"));
+    Airline airline = new Airline("abc");
+    Flight flight1 = new Flight(argsListWithAllArgsSampler1);
+    Flight flight2 = new Flight(argsListWithAllArgsSampler2);
 
     @Test
     void canCreateXmlDumperObject() throws IOException {
@@ -26,5 +32,14 @@ public class XmlDumperTest {
         testFile.deleteOnExit();
         XmlDumper dumper = new XmlDumper(new FileWriter(testFile));
         assertThat(dumper.createXmlDocument(), instanceOf(Document.class));
+    }
+
+    @Test
+    void canDumpAirlineToXmlDocument() throws IOException {
+        testFile.deleteOnExit();
+        airline.addFlight(flight1);
+        airline.addFlight(flight2);
+        XmlDumper dumper = new XmlDumper(new FileWriter(testFile));
+        dumper.dump(airline);
     }
 }
