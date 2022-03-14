8ca22011b2e6c03b0b186eaae596e199de2c2023
minavu <mina8@pdx.edu>
2022-03-14 15:17:48 -0700

    Created sum function for calculator.

--- airline-android/app/src/main/java/edu/pdx/cs410j/mina8/CalculatorActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410j/mina8/CalculatorActivity.java
@@ -3,6 +3,9 @@ package edu.pdx.cs410j.mina8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    }
+
+    public void calculateSum(View view) {
+        EditText leftOperand = findViewById(R.id.leftOperand);
+        EditText rightOperand = findViewById(R.id.rightOperand);
+
+        String left = leftOperand.getText().toString();
+        String right = rightOperand.getText().toString();
+
+        int sum = Integer.parseInt(left) + Integer.parseInt(right);
+
+        EditText sumResult = findViewById(R.id.sumResult);
+        sumResult.setText(String.valueOf(sum));
     }
 }
