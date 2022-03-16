60de77d9958e4db8985554a57ff2524c68f545cf
minavu <mina8@pdx.edu>
2022-03-15 21:18:32 -0700

    Search spinners working properly

--- airline-android/app/src/main/java/edu/pdx/cs410j/mina8/SearchActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410j/mina8/SearchActivity.java
@@ -4,20 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.TreeSet;
 
 import edu.pdx.cs410J.AirportNames;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements OnItemSelectedListener {
     private TreeSet<Airline> hub;
     private ArrayAdapter<String> airlines;
+    private ArrayAdapter<String> sources;
+    private ArrayAdapter<String> destinations;
     private ArrayAdapter<String> flights;
+
+    Airline airline = null;
+    String source = null;
+    String destination = null;
+
+    Spinner airlineSpinner = null;
+    Spinner sourceSpinner = null;
+    Spinner destSpinner = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +37,32 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
 
-        // Spinner element
-        Spinner spinner = (Spinner) findViewById(R.id.airlineSpinner);
 
-        // Spinner click listener
-//        spinner.setOnItemSelectedListener(this);
-
+        airlineSpinner = findViewById(R.id.airlineSpinner);
         this.airlines = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
-        this.airlines.add("---Select an airline---");
+        this.airlines.add("---All airlines---");
         this.airlines.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(this.airlines);
+        airlineSpinner.setAdapter(this.airlines);
+        // Spinner click listener
+        airlineSpinner.setOnItemSelectedListener(this);
+
+        sourceSpinner = findViewById(R.id.sourceSpinner);
+        this.sources = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        this.sources.add("---All sources---");
+        this.sources.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sourceSpinner.setAdapter(this.sources);
+        // Spinner click listener
+        sourceSpinner.setOnItemSelectedListener(this);
+
+        destSpinner = findViewById(R.id.destSpinner);
+        this.destinations = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        this.destinations.add("---All destinations---");
+        this.destinations.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        destSpinner.setAdapter(this.destinations);
+        // Spinner click listener
+        destSpinner.setOnItemSelectedListener(this);
+
+
 
         this.flights = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         ListView listView = findViewById(R.id.flightsList);
@@ -50,12 +78,6 @@ public class SearchActivity extends AppCompatActivity {
                 });
             }
         }
-//        if (data.hasExtra(MainActivity.AIRLINE_SEARCH)) {
-//            Airline airline = (Airline) data.getSerializableExtra(MainActivity.AIRLINE_SEARCH);
-//            airline.getFlights().forEach(flight -> {
-//                flights.add(flight);
-//            });
-//        }
     }
 
     public StringBuilder prettyPrintFlight(Flight flight) {
@@ -70,14 +92,85 @@ public class SearchActivity extends AppCompatActivity {
         String duration = flight.getFlightDuration();
 
         String EMPTY_STRING = "";
-        StringBuilder stringBuilder = new StringBuilder("\n");
-        stringBuilder.append(String.format("%s Airline\n", airline));
+        StringBuilder stringBuilder = new StringBuilder("");
+        stringBuilder.append(String.format("%s Airline\n\n", airline));
         stringBuilder.append(String.format("%10s%-4s->%4s\n", EMPTY_STRING, src, dest));
-        stringBuilder.append(String.format("%10s%8s%s\n", EMPTY_STRING, "Flight", flightNumber));
-        stringBuilder.append(String.format("%10s%8s%s at %s\n", EMPTY_STRING, "Depart", srcName, departure));
-        stringBuilder.append(String.format("%10s%8s%s at %s\n", EMPTY_STRING, "Arrive", destName, arrival));
-        stringBuilder.append(String.format("%10s%8s%s\n", EMPTY_STRING, "Duration", duration));
+        stringBuilder.append(String.format("%10s%-10s%s\n", EMPTY_STRING, "Flight", flightNumber));
+        stringBuilder.append(String.format("%10s%-10s%s at %s\n", EMPTY_STRING, "Depart", srcName, departure));
+        stringBuilder.append(String.format("%10s%-10s%s at %s\n", EMPTY_STRING, "Arrive", destName, arrival));
+        stringBuilder.append(String.format("%10s%-10s%s\n", EMPTY_STRING, "Duration", duration));
 
         return stringBuilder;
+    }
+
+    public void clearBtnHandler(View view) {
+        airlineSpinner.setSelection(0);
+
+        this.sources.clear();
+        this.sources.add("---All sources---");
+        sourceSpinner.setSelection(0);
+
+        this.destinations.clear();
+        this.destinations.add("---All destinations---");
+        destSpinner.setSelection(0);
+    }
+
+    public void searchBtnHandler(View view) {
+        Toast.makeText(this, "SEARCH! " + airline.getName() + ", " + source + ", " + destination, Toast.LENGTH_LONG).show();
+
+        this.flights.clear();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int index, long id) {
+        String selected = parent.getSelectedItem().toString();
+        if (selected.contains("All")) {
+            return;
+        }
+        switch (parent.getId()) {
+
+            case R.id.airlineSpinner:
+                clearBtnHandler(view);
+                airlineSpinner.setSelection(index);
+                for (Airline a : hub) {
+                    if (a.getName().equalsIgnoreCase(selected)) {
+                        airline = a;
+                        airline.getFlights().forEach(flight -> {
+                            this.sources.add(flight.getSource());
+                        });
+                        break;
+                    }
+                }
+                break;
+
+
+            case R.id.sourceSpinner:
+                this.destinations.clear();
+                this.destinations.add("---All destinations---");
+                destSpinner.setSelection(0);
+                source = selected;
+                airline.getFlights().forEach(flight -> {
+                    if (flight.getSource().equalsIgnoreCase(selected)) {
+                        this.destinations.add(flight.getDestination());
+                    }
+                });
+                break;
+
+
+            case R.id.destSpinner:
+                destination = selected;
+                break;
+
+
+            default:
+                Toast.makeText(this, selected + " was sent to default branch of switch statement", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
